b5bc4840a37830072c3ce1ad86c6ce86acd4a21c
johnatrootdynamics <john.abraham@root-dynamics.com>
2024-02-19 00:52:33 -0500

    asd

--- app.py
+++ app.py
@@ -528,6 +528,7 @@ def car_inspection():
 
     
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
     app.secret_key = 'supersecret'
+    app.run(debug=True, host='0.0.0.0', port=80)
+    
     #app.run(debug=True)
