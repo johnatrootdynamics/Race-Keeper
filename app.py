7febb7749db1a38e2de96d22840e15c4c769b97d
johnatrootdynamics <john.abraham@root-dynamics.com>
2024-02-19 22:07:54 -0500

    kj

--- app.py
+++ app.py
@@ -531,6 +531,6 @@ def car_inspection():
 
     
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80,ssl_context='adhoc')
+    app.run(debug=True, host='0.0.0.0', port=80)
     
     #app.run(debug=True)
